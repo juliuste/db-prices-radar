0263123be5557c85737890d165496cafaa7c5293
Julius Tens <mail@juliustens.eu>
2018-03-24 00:00:54 +0100

    exitCode 1

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const moment = require('moment')
 const ndjson = require('ndjson')
 const map = require('through2-map').obj
 
+const showError = (e) => {
+	showError(e)
+	process.exitCode = 1
+}
+
 const stream = () => map(x => x) // … xD
 
 const main = async () => {
@@ -14,13 +19,13 @@ const main = async () => {
 
 	for(let route of config.routes){
 		// fetch data for route
-		let routeResults = await (fetchRoute(route).catch(console.error))
+		let routeResults = await (fetchRoute(route).catch(showError))
 		for(let res of (routeResults || [])) results.write(res)
 
 		// (opt) fetch data for return route
 		if(config.returnTrips){
 			route = {origin: route.destination, destination: route.origin}
-			let routeResults = await (fetchRoute(route).catch(console.error))
+			let routeResults = await (fetchRoute(route).catch(showError))
 			for(let res of (routeResults || [])) results.write(res)
 		}
 	}
@@ -28,6 +33,6 @@ const main = async () => {
 	results.end()
 }
 
-main().catch(console.error)
+main().catch(showError)
 
 module.exports = main
